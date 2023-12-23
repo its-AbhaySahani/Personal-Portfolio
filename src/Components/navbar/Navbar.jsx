5a168df361a32e9d72157803299bf5bea90dc41e
Abhay Sahani <abhay.cs.777@gmail.com>
2023-12-24 00:58:50 +0530

    framer motion

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -10,8 +10,9 @@ const Navbar = () => {
             <div className="Social">
                 <a href="#"><img src="/instagram.png" alt="" /></a>
                 <a href="#"><img src="/facebook.png" alt="" /></a>
-                <a href="#"><img src="/youtube.png" alt="" /></a>
-                <a href="#"><img src="/dribbble.png" alt="" /></a>
+                <a href="#"><img src="/Youtube.png" alt="" /></a>
+                <a href="#"><img src="/twitter.png" alt="" /></a>
+                <a href="#"><img src="/linkedin.png" alt="" /></a>
             </div>
         </div>
     </div>
